83446c598cd27f5e0f22006822cd1983bb4ccd71
Matthew Jordan <5577434+todo-make-username@users.noreply.github.com>
2023-12-27 16:58:28 -0600

    String Helper Unit test completed

---
+++ tests/unit/Util/StringHelperTest.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use TodoMakeUsername\ObjectHelpers\Util\StringHelper;
+
+class StringHelperTest extends TestCase
+{
+	public static function stringHelperDataProvider()
+	{
+		return [
+			[
+				'String',
+				'abc',
+				true,
+			],[
+				'Int',
+				'123',
+				true,
+			],[
+				'Bool',
+				true,
+				true,
+			],[
+				'float',
+				'123.123',
+				true,
+			],[
+				'Object',
+				new class() {},
+				false,
+			],[
+				'Object w/ toString',
+				new class() {
+					public function __toString()
+					{
+						return 'I\'m a string!';
+					}
+				},
+				true,
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider stringHelperDataProvider
+	 */
+	public function testIsStringCompatible(string $test_name, $value, bool $expected)
+	{
+		$result = StringHelper::isStringCompatible($value);
+		$this->assertSame($expected, $result);
+	}
+}
